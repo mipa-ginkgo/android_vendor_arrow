7925999ac85c1bb8da210527a7c4a4c8ab57839e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-23 10:43:00 +0530

    overlay: Switch to PropImitationHooks for spoofing
    Use marlin props from https://github.com/hentaiOS/platform_frameworks_base/commit/32cd021753626ae2dc0f72d222ac4fd734f3a8aa
    
    Change-Id: I00ce0b3f7574a4b4e1147147cc1b8521f215ade2

--- config/props.mk
+++ config/props.mk
@@ -79,28 +79,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Enable SystemUIDialog volume panel
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     sys.fflag.override.settings_volume_panel_in_systemui=true
-
-# Spoof props for Google Play Services and SafetyNet
-ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
-else
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
-endif
-
-ifeq ($(PRODUCT_OVERRIDE_GMS_PRODUCT),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_product=walleye
-else
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_product=$(PRODUCT_OVERRIDE_GMS_PRODUCT)
-endif
-
-ifeq ($(PRODUCT_OVERRIDE_GMS_DEVICE),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_device=walleye
-else
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_device=$(PRODUCT_OVERRIDE_GMS_DEVICE)
-endif
-
-ifeq ($(PRODUCT_OVERRIDE_GMS_MODEL),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_model=Pixel 2
-else
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_model=$(PRODUCT_OVERRIDE_GMS_MODEL)
-endif
